1aa11d03e8c15224786ded7d4baca57eccc907ed
Tiaralstari <142280838+Tiaralstari@users.noreply.github.com>
2023-08-15 15:37:56 +0700

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,11 @@
+import random
+
+class tebakangka:
+    def_init_(self):
+    self_tebakan =0
+    self_jawaban = random.randint(1,10)
+
+def cek(self):
+    if self.tebakan ==self.jawaban
+    return True
+    return False
